e3c6ab44c85ca08cfe1832e21e7f5e0689fc7666
Reynaldev <ReynDev@proton.me>
2025-12-24 03:13:49 +0700

    Minor fixes

--- MLproject/modelling.py
+++ MLproject/modelling.py
@@ -1,22 +1,19 @@
-import sys
 import mlflow
 import pandas as pd
 
 from sklearn import svm
 from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
-from sklearn.feature_extraction.text import TfidfTransformer, TfidfVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 
+mlflow.set_tracking_uri("http://127.0.0.1:5000/")
+mlflow.set_experiment("Workflow CI")
 mlflow.sklearn.autolog()
 
 if __name__ == "__main__":
     data = pd.read_csv("prdectid_preprocessing.csv")
 
     X_train, X_test, y_train, y_test = train_test_split(data['review_akhir'], data['Sentiment'], test_size=0.2, random_state=42)
-
-    params = {
-        "C": float(sys.argv[1] if len(sys.argv) > 1 else 1.0)
-    }
 
     with mlflow.start_run():
         vectorizer = TfidfVectorizer(
@@ -28,8 +25,17 @@ if __name__ == "__main__":
         train_ds = vectorizer.fit_transform(X_train)
         test_ds = vectorizer.transform(X_test)
 
-        model = svm.SVC(C=params["C"], kernel="linear")
+        model = svm.SVC(
+            kernel="linear", 
+            random_state=42
+        )
         model.fit(train_ds, y_train)
 
         prediction = model.predict(test_ds)
         print(classification_report(y_test, prediction, target_names=["positive", "negative"]))
+
+        mlflow.sklearn.log_model(
+            sk_model=model,
+            artifact_path="svm_hyperparam_model",
+            registered_model_name="sentiment-analysis"
+        )
